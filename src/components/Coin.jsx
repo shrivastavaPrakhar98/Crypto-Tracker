1dd6d2e4d6a38a68d05a5c3f43b480ba297169df
shrivastavaPrakhar98 <106315068+shrivastavaPrakhar98@users.noreply.github.com>
2022-06-17 19:32:53 +0530

    Deployed website

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './coin.css'
 
-const Coin = ({ name, image, symbol, price, volume, rank, price_change , market_cap }) => {
+const Coin = ({ name, image, symbol, price, volume, rank, price_change, market_cap }) => {
     return (
         <>
             <div className="coin-container">
@@ -15,13 +15,13 @@ const Coin = ({ name, image, symbol, price, volume, rank, price_change , market_
                         <p className="coin-price">${price} </p>
                         <p className="coin-volume">${volume} </p>
                         <p className="coin-rank">{rank} </p>
-                        {
+                        { /*
                             price_change < 0 ? (
-                                <p className="coin-percentage-red"> {price_change.toFixed(2)}% </p>
+                               <p className="coin-percentage-red"> {price_change.toFixed(2)}% </p>
                             ) : (
                                     <p className="coin-percentage-green"> {price_change.toFixed(2)}% </p>
                                 )
-                        }
+                            */}
                         <p className="market_cap">MKC: {market_cap}  </p>
                     </div>
                 </div>
@@ -30,6 +30,6 @@ const Coin = ({ name, image, symbol, price, volume, rank, price_change , market_
     )
 }
 
- 
+
 
 export default Coin;
--- src/components/Tracker.jsx
+++ src/components/Tracker.jsx
@@ -13,7 +13,7 @@ const Tracker = () => {
         axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').then((res) => {
 
             setCoins(res.data);
-            //console.log(res.data);
+            console.log(res.data);
         }).catch(error => console.log(error));
     }, []);
 
